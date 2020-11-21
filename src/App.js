b170e5043eb78d3ec1fd5907a7a5d7ca94cc4070
Vishesh Singh <VisheshSingh16@gmail.com>
2020-11-21 15:03:37 -0600

    API request

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import Map from './components/Map';
 
 function App() {
-  return <Map />;
+  const [eventData, setEventData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      setLoading(true);
+      const res = await fetch(
+        `https://eonet.sci.gsfc.nasa.gov/api/v2.1/events`
+      );
+      const { events } = await res.json();
+      setEventData(events);
+      setLoading(true);
+    };
+    fetchEvents();
+    console.log(eventData);
+  }, []);
+
+  return (
+    <div>
+      <Map />
+    </div>
+  );
 }
 
 export default App;
